198974740bc4ea94030e426544a72d0d15962d62
Trev-D-Dev <dunn.trevor@pm.me>
2025-08-28 15:39:26 -0400

    Started course, currently on step 8 of lesson 5

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,52 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+
+	// Max Memory of 10MB, bit shifted left 20 times to get number of bytes
+	const maxMemory = 10 << 20
+
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+
+	}
+
+	// Get the image data from the form
+	fileData, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+
+	}
+	mediaType := fileHeader.Header.Get("Content-Type")
+
+	// Read image data into a byte slice
+	imageData, err := io.ReadAll(fileData)
+	if err != nil {
+
+	}
+
+	// Get video's metadata from the SQLite database
+	dbVideo, err := cfg.db.GetVideo(videoID)
+	if userID != dbVideo.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Access Denied", err)
+		return
+	}
+
+	// Save the thumbnail to the global map
+	thumb := thumbnail{
+		data:      imageData,
+		mediaType: mediaType,
+	}
+	videoThumbnails[videoID] = thumb
+
+	// Update the video metadata so it has a new thumbnail URL
+	thumbURL := fmt.Sprintf("/api/thumbnails/%v", videoID)
+	dbVideo.ThumbnailURL = &thumbURL
+	err = cfg.db.UpdateVideo(dbVideo)
+	if err != nil {
+
+	}
 
 	respondWithJSON(w, http.StatusOK, struct{}{})
 }
